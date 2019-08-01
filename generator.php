77f6e0d961604177e6e4d00c74728c6fd68a7b7a
Alexander <g.a.androidjc2@ya.ru>
2019-08-01 11:16:37 +0400

    Добавили полосочки

--- generator.php
+++ generator.php
@@ -8,5 +8,24 @@ $bgColor = imagecolorallocate($img, 32, 64, 85);
 // и его заливка. Заливаем от указанных координат
 imagefill($img, 0, 0, $bgColor);
 
+// Цвет текста и линий
+$white = imagecolorallocate($img, 255, 255, 255);
+
+// Верхняя линия
+imagerectangle($img, 100, 80, 700, 90, $white);
+imagefilltoborder($img, 101, 81, $white, $white);
+
+// левая линия
+imagerectangle($img, 100, 90, 110, 400, $white);
+imagefilltoborder($img, 101, 91, $white, $white);
+
+// нижняя линия
+imagerectangle($img, 100, 400, 700, 410, $white);
+imagefilltoborder($img, 101, 401, $white, $white);
+
+// правая линия
+imagerectangle($img, 690, 400, 700, 180, $white);
+imagefilltoborder($img, 691, 201, $white, $white);
+
 imagepng($img, 'result.png');
 
